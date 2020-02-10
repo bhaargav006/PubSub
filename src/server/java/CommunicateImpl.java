01cd7463352bfed16aeb30708f9a990557e7184f
mehta-garima <garimamehta2194@gmail.com>
2020-02-10 12:29:23 -0600

    Added Subscribe and Unsubscribe functionalities

--- src/server/java/CommunicateImpl.java
+++ src/server/java/CommunicateImpl.java
@@ -12,8 +12,10 @@ public class CommunicateImpl extends UnicastRemoteObject implements Communicate 
     protected CommunicateImpl() throws RemoteException {
     }
 
-    List<String> clientList = new ArrayList<>();
-    Map<String,String> clientSubscription = new HashMap<>();
+    List<String> clientList = new ArrayList<String>();
+    Map<String,String> clientSubscription = new HashMap<String, String>();
+
+    Map<String, ArrayList<String>> clientSubscriptionList = new HashMap<String, ArrayList<String>>();
     String currArticle;
 
     public boolean join(String IP, int Port) throws RemoteException {
@@ -44,12 +46,53 @@ public class CommunicateImpl extends UnicastRemoteObject implements Communicate 
         return true;
     }
 
-    public boolean subscribe(String IP, int Port, String Article) throws RemoteException {
-        return false;
+    public boolean subscribe(String IP, int Port, String article) throws RemoteException {
+        // Map currently take the key as client IP address. I think we can use the key as type/originator/org value.
+        // Need to discuss
+        try{
+            String clientIP = RemoteServer.getClientHost();
+            String[] fieldValues = article.split(";");
+            for(int i = 0; i < fieldValues.length; i++){
+                if(fieldValues[i] != ""){
+                    if(!clientSubscriptionList.containsKey(clientIP)){
+                        clientSubscriptionList.put(clientIP, new ArrayList<String>());
+                    }
+                    clientSubscriptionList.get(clientIP).add(fieldValues[i]);
+                }
+            }
+        } catch (ServerNotActiveException e) {
+            System.out.println("Could not get Client IP");
+            return false;
+        }
+
+        return true;
     }
 
-    public boolean unSubscribe(String IP, int Port, String Article) throws RemoteException {
-        return false;
+    public boolean unSubscribe(String IP, int port, String article) throws RemoteException {
+        /*Key being type/originator/org makes sense because you can unsubscribe from a particular topic but
+        can still be subscribed to another one.
+        That's my understanding. We can discuss more on this.*/
+        try{
+            String clientIP = RemoteServer.getClientHost();
+            String[] fieldValues = article.split(";");
+            for(int i = 0; i < fieldValues.length; i++){
+                if(fieldValues[i] != ""){
+                    if(clientSubscriptionList.containsKey(clientIP)){
+                        ArrayList<String> clientSubscribedArticles = clientSubscriptionList.get(clientIP);
+                        clientSubscribedArticles.remove(fieldValues[i]);
+                        clientSubscriptionList.put(clientIP, clientSubscribedArticles);
+                    }
+                    else{
+                        System.out.println("Client is not subscribed!");
+                        return false;
+                    }
+                }
+            }
+        } catch (ServerNotActiveException e) {
+            System.out.println("Could not get Client IP");
+            return false;
+        }
+        return true;
     }
 
     public boolean publish(String Article, String IP, int Port) throws RemoteException {

74f9eeba02761a2a5a747a811da2083565111d0a
mehta-garima <garimamehta2194@gmail.com>
2020-02-19 13:44:04 -0600

    Ping functionality added.

--- src/client/java/Client.java
+++ src/client/java/Client.java
@@ -6,33 +6,45 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 
     public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException, UnknownHostException, SocketException, InterruptedException {
 
         //Needs to get Server IP and Port from Registry Server
-
         new PublishedClient().start();
-        String ip = CommunicateHelper.getIP();
+        final String ip = CommunicateHelper.getIP();
+        final String registryIP = args[0];
 
-        Map<String,Integer>  ipAddrAndPort = CommunicateHelper.getServerList(args[0], ip);
-        Communicate comm = CommunicateHelper.connectToGroupServer(ipAddrAndPort, ip);
-        if(comm==null){
-            System.out.println("Not able to connect to Group server");
-            return;
+        Communicate comm = null;
+        int i = 1;
+        while (true) {
+            Thread.sleep(10000);
+            Map<String, Integer> ipAddrAndPort = CommunicateHelper.getServerList(args[0], ip);
+            comm = CommunicateHelper.connectToGroupServer(ipAddrAndPort, ip);
+            System.out.println("Got IP address for " + i + "st time");
+            if (comm == null) {
+                System.out.println("Not able to connect to Group server");
+                return;
+            }
+            comm.subscribe(InetAddress.getLocalHost().getHostAddress(), 1098, "Science;;UMN;");
+            Thread.sleep(20000);
+            //Wrong unsubscribe request
+            comm.unSubscribe(InetAddress.getLocalHost().getHostAddress(), 1098, "Lifestyle;Soumya;UMN;");
+            comm.subscribe(InetAddress.getLocalHost().getHostAddress(), 1098, "Sports;;;");
+
+            comm.publish("Science;;UMN;Some message", InetAddress.getLocalHost().getHostAddress(), 1098);
+            //    comm.leave(ia.getHostAddress(),9999);
+
+            PingThread thread = new PingThread(comm);
+            thread.start();
+            thread.join();
+            i++;
+            System.out.println("Main thread restarted for connecting to new server.");
         }
-//        comm.subscribe(InetAddress.getLocalHost().getHostAddress(),1098, "Science;;UMN;");
-//        Thread.sleep(20000);
-//        //Wrong unsubscribe request
-//        comm.unSubscribe(InetAddress.getLocalHost().getHostAddress(),1098, "Lifestyle;Soumya;UMN;");
-        comm.subscribe(InetAddress.getLocalHost().getHostAddress(), 1098, "Sports;;;");
-
-        //comm.publish("Science;;UMN;Some message", InetAddress.getLocalHost().getHostAddress(), 1098);
-        //    comm.leave(ia.getHostAddress(),9999);
-
     }
 }
--- src/client/java/TestClient1.java
+++ src/client/java/TestClient1.java
@@ -6,10 +6,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TestClient1 {
 
@@ -18,20 +15,34 @@ public class TestClient1 {
         //Needs to get Server IP and Port from Registry Server
 
         new PublishedClient().start();
-        String ip = CommunicateHelper.getIP();
+        final String ip = CommunicateHelper.getIP();
+        final String registryIP = args[0];
 
-        Map<String,Integer>  ipAddrAndPort = CommunicateHelper.getServerList(args[0], ip);
-        Communicate comm = CommunicateHelper.connectToGroupServer(ipAddrAndPort, ip);
-        if(comm==null){
-            System.out.println("Not able to connect to Group server");
-            return;
+        Communicate comm = null;
+        int i = 1;
+        while(true) {
+            Thread.sleep(10000);
+            Map<String,Integer>  ipAddrAndPort = CommunicateHelper.getServerList(args[0], ip);
+            comm = CommunicateHelper.connectToGroupServer(ipAddrAndPort, ip);
+            System.out.println("Got IP address for " + i + "st time");
+            if(comm==null){
+                System.out.println("Not able to connect to Group server");
+                return;
+            }
+            comm.subscribe(InetAddress.getLocalHost().getHostAddress(),1098, "Science;;UMN;");
+            //To test with other clients
+            Thread.sleep(30000);
+            comm.publish("Sports;Garima;UMN;FootballNews", InetAddress.getLocalHost().getHostAddress(), 1098);
+            //To test with other clients
+            Thread.sleep(10000);
+            comm.publish("Sports;;;HealthyLife", InetAddress.getLocalHost().getHostAddress(), 1098);
+
+            PingThread thread = new PingThread(comm);
+            thread.start();
+            thread.join();
+            i++;
+            System.out.println("Main thread restarted for connecting to new server.");
         }
-        comm.subscribe(ip,1098, "Science;;UMN;");
-        Thread.sleep(40000);
-        comm.publish("Sports;Garima;UMN;FootballNews", ip, 1098);
-        Thread.sleep(10000);
-        comm.publish("Sports;;;HealthyLife", ip, 1098);
-
         //    comm.leave(ia.getHostAddress(),9999);
 
     }
